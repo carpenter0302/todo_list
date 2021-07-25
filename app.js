00650c5f3012c2e025a3071ccfbbacb5f39e9e54
高廷宇 <tim860302@gmail.com>
2021-07-25 18:20:26 +0800

    連線資料庫並建立 Todo Model

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res)=>{
+
+const mongoose = require('mongoose')   // 載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true})  // 設定連線到 mongoDB
+const db = mongoose.connection  // 取得資料庫連線狀態
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {       //連線成功只會發生一次，所以這裡使用 once
+  console.log('mongodb connected!')
+})
+
+
+app.get('/', (req, res) => {
   res.send("hello world")
 })
 
