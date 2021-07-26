eab17e61d39239ad7316ba3805b2160f6c98bd01
高廷宇 <tim860302@gmail.com>
2021-07-26 14:06:30 +0800

    瀏覽所有 To-do (首頁)

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = 3000
 
 const exphbs = require('express-handlebars');
 
+const Todo = require('./models/todo')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //定義要使用的樣板引擎
 app.set('view engine', 'hbs')
 
 const mongoose = require('mongoose')   // 載入 mongoose
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true})  // 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })  // 設定連線到 mongoDB
 const db = mongoose.connection  // 取得資料庫連線狀態
 // 連線異常
 db.on('error', () => {
@@ -21,7 +23,10 @@ db.once('open', () => {       //連線成功只會發生一次，所以這裡使
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(items => res.render('index', { todos: items })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(port, () => {
